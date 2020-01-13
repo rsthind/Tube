e778eb91d074e52f61b51be0b7c1423fb3204740
Adi Singh <adi.singh901@gmail.com>
2020-01-12 22:50:46 -0500

    Fixed registration 2

--- app/src/main/java/com/example/firebasetest/RegistrationActivity2.java
+++ app/src/main/java/com/example/firebasetest/RegistrationActivity2.java
@@ -3,9 +3,14 @@ package com.example.firebasetest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,35 +21,63 @@ import java.util.ArrayList;
 
 public class RegistrationActivity2 extends AppCompatActivity {
     private Button create_account;
-    private final String[] CLASSES = {"CS1331", "MATH1554", "ENGL1102", "INTA1200", "ECON2106", "CS2701"};
-    private MultiAutoCompleteTextView class_input;
-
+    private Button add_class;
     private FirebaseDatabase db;
+    private AutoCompleteTextView actv;
     private DatabaseReference dbref;
-
     private String sGTID;
     private String sFname;
     private String sEmail;
     private String sMajor;
     private String sPassword;
-    private ArrayList<String> sCourses_List = new ArrayList<>();
+    private ArrayList<String> courses_list = new ArrayList<>();
+    private ListView listView;
+    private final String[] CLASSES = {"CS1331", "MATH1554", "ENGL1102","INTA1200","ECON2106", "CS2701"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration2);
-        create_account = findViewById(R.id.create_account);
-        class_input = findViewById(R.id.mactv);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, CLASSES);
-        class_input.setAdapter(adapter);
-        class_input.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        create_account = findViewById(R.id.create_account);
+        add_class = findViewById(R.id.add_class);
+        actv = findViewById(R.id.course_actv);
+        listView = findViewById(R.id.lv);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, CLASSES);
+        actv.setAdapter(adapter);
+
+        add_class.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = actv.getText().toString();
+
+                //check to make sure entered item is only part of entered list
+                boolean isValid = false;
+                for (int a = 0; a < adapter.getCount();a++) {
+                    String temp = adapter.getItem(a);
+                    if (input.compareTo(temp) == 0) {
+                        isValid = true;
+                        break;
+                    }
+                }
+
+                if (isValid && notRepeat(input)) {
+                    courses_list.add(input);
+                    updateList();
+                    actv.setText("");
+                } else {
+                    Toast.makeText(RegistrationActivity2.this,"Please Enter a Valid Course ID",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         create_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addStudents();
-                class_input.setText("");
                 startActivity(new Intent(RegistrationActivity2.this, UserOrTutor.class));
             }
         });
@@ -56,11 +89,28 @@ public class RegistrationActivity2 extends AppCompatActivity {
         sEmail = getIntent().getStringExtra("email");
         sMajor = getIntent().getStringExtra("major");
         sPassword = getIntent().getStringExtra("password");
-        sCourses_List.add(class_input.getText().toString().trim());
 
         dbref = FirebaseDatabase.getInstance().getReference("users");
         String id = dbref.push().getKey();
-        User add = new User(sGTID, sFname, sEmail, sMajor, sPassword, sCourses_List);
+        User add = new User(sGTID, sFname, sEmail, sMajor, sPassword, courses_list);
         dbref.child(id).setValue(add);
+    }
+
+    public void updateList() {
+        ArrayAdapter<String> list_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                courses_list);
+        listView.setAdapter(list_adapter);
+    }
+
+    public boolean notRepeat(String input) {
+        for (int a = 0; a < courses_list.size(); a++) {
+            if (input.compareTo(courses_list.get(a)) == 0) {
+                Toast.makeText(RegistrationActivity2.this,"Cannot Repeat Course Entry",
+                        Toast.LENGTH_SHORT).show();
+                actv.setText("");
+                return false;
+            }
+        }
+        return true;
     }
 }
